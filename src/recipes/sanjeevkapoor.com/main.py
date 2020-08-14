fbf497f4b4b5398a1343fe533e99e7179e4723f6
Param Kapur <paramkapur2002@gmail.com>
2020-08-14 12:38:53 +0530

    added sanjeev kapoor dump

--- src/recipes/sanjeevkapoor.com/main.py
+++ src/recipes/sanjeevkapoor.com/main.py
@@ -1,3 +1,4 @@
+from itertools import islice
 import requests
 import json
 from bs4 import BeautifulSoup
@@ -5,12 +6,13 @@ from pandas import DataFrame
 from pprint import pprint
 
 
+
 def get_metadata_from_recipe_page(soup):
     main_ingredients, region, course = [i.find('span', {'class': 'clrblack'}).text for i in soup.findAll('h4', {'class': 'clrlightgray'})]
 
     r = {'veg_nonveg' : soup.find('a', {'class': 'vegicon'}).img['src'].split('Images/')[-1].split('-')[0],
         'main_ingredients': main_ingredients,
-        'region': region, 
+        'region': region,
         'course': course}
 
     return r
@@ -23,7 +25,7 @@ def get_data_from_print_page(soup):
         for i in list_items:
             x.append(i.text.strip())
         return x
-    
+
 
     r = {'title': soup.find('h3', {'id': 'headingh1'}).text.split('- SK')[0].strip(),
         'description': soup.find('div', {'class': 'shortdisc', 'itemprop': 'description'}).text.strip(),
@@ -33,14 +35,14 @@ def get_data_from_print_page(soup):
         'ingredients': get_ingredients(soup),
         'method': [i.text.strip() for i in soup.findAll('span', {'itemprop': 'recipeInstructions'})],
         }
-    
+
     return r
 
 count = 0
 
 with open('data.json', 'w+') as out:
     with open('links.txt', 'r') as f:
-        for link in f:
+        for link in islice(f, 2086, None):
             print('getting66 r ' + link + '   number:  ' + str(count))
 
             r = {}
@@ -50,7 +52,10 @@ with open('data.json', 'w+') as out:
             name = soup.find('div', {'class': 'htmlbread hidden-xs'}).findAll('a')[-1].text
             print_page_url = 'https://www.sanjeevkapoor.com/recipe/printrecipe.aspx?recipe_name=' + requests.utils.quote(name, safe='')
             print_page_soup = BeautifulSoup(requests.get(print_page_url).content, 'html.parser')
-            r.update(get_data_from_print_page(print_page_soup))
-            r.update(get_metadata_from_recipe_page(soup))
+            try:
+                r.update(get_data_from_print_page(print_page_soup))
+                r.update(get_metadata_from_recipe_page(soup))
+            except:
+                pass
             out.write(json.dumps(r) + ',' + '\n')
             count += 1
